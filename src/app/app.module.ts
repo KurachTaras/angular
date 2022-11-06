9f826b247a2a1161a18eff51a62e5a311c634d06
KurachTaras <moonlight.jst@gmail.com>
2022-11-06 11:50:02 +0200

    Lesson_4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+      BrowserModule,
+      HttpClientModule,
+      ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
